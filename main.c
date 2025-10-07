9d26396e0233212e73e8894aa7b9c70c81d56e79
yaelCAV <yael.cavaille-voisin@ensea.fr>
2025-10-07 11:28:23 +0200

    free3is fonctionnel

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 /*---------------------------------------------------------------------------------*/
 typedef struct HEADER_TAG {
     struct HEADER_TAG *ptr_next; /* pointe sur le prochain bloc libre */
+    struct HEADER_TAG *ptr_previous;
     size_t bloc_size; /* taille du memory bloc en octets*/
     long magic_number; /* 0x0123456789ABCDEFL */
 } HEADER;
@@ -15,11 +16,10 @@ typedef struct HEADER_TAG {
 HEADER *free_memory = NULL;
 
 void *malloc3is(size_t size) {
-
     void *request = sbrk(sizeof(HEADER) + size + sizeof(MAGIC_NUMBER));
     if (request == (void *) -1) {
-            return NULL; // sbrk failed.
-        }
+        return NULL; // sbrk failed.
+    }
 
     //initialisation du header + block
     HEADER *new_header = request;
@@ -28,13 +28,17 @@ void *malloc3is(size_t size) {
     *((long long *) (new_header + sizeof(HEADER) + size)) = MAGIC_NUMBER;
 
 
-
     return ++new_header; //typed pointer
 }
 
 
-void  free3is(void *ptr) {
-
+void free3is(void *ptr) {
+    if (free_memory == NULL) {
+        free_memory = --ptr;
+    } else {
+        free_memory->ptr_previous = free_memory;
+        free_memory->ptr_next = (HEADER *) (--ptr);
+    }
 }
 
 int main(void) {
@@ -43,5 +47,9 @@ int main(void) {
     void *p2 = malloc3is(0);
     printf("%p\n", p);
     printf("%p\n", p2);
-    printf("%i\n", sizeof(HEADER) + sizeof(MAGIC_NUMBER));
+    printf("%lu\n", sizeof(HEADER) + sizeof(MAGIC_NUMBER));
+
+    printf("%p\n",free_memory );
+    free3is(p);
+    printf("%p\n",free_memory );
 }
