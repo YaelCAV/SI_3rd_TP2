8df3ca9625d80da997ff48842031ef4b87013b63
yaelCAV <yael.cavaille-voisin@ensea.fr>
2025-10-07 10:25:26 +0200

    Malloc basique fonctionnel avec header

---
+++ main.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+#include <stdlib.h>
+#define MAGIC_NUMBER 0X0123456789ABCDEFL
+#define BLOCK_SIZE 1024
+
+/*---------------------------------------------------------------------------------*/
+typedef struct HEADER_TAG {
+    struct HEADER_TAG *ptr_next; /* pointe sur le prochain bloc libre */
+    size_t bloc_size; /* taille du memory bloc en octets*/
+    long magic_number; /* 0x0123456789ABCDEFL */
+} HEADER;
+
+
+HEADER *free_memory = NULL;
+
+void *malloc3is(size_t size) {
+    void *p = sbrk(0);
+    void *request = sbrk(sizeof(HEADER) + size + sizeof(MAGIC_NUMBER));
+    HEADER *new_header = (HEADER *) request;
+    new_header->magic_number = MAGIC_NUMBER;
+    *((long long *) (new_header + sizeof(HEADER) + size)) = MAGIC_NUMBER;
+
+    if (request == (void *) -1) {
+        return NULL; // sbrk failed.
+    }
+    assert(p == new_header);
+    return new_header;
+}
+
+int main(void) {
+    printf("Hello, World!\n");
+    void *p = malloc3is(0);
+    void *p2 = malloc3is(0);
+    printf("%p\n", p);
+    printf("%p\n", p2);
+}
---
+++ main.h
@@ -0,0 +1,14 @@
+//
+// Created by yael on 10/7/25.
+//
+
+#ifndef TPMALLOC_MAIN_H
+#define TPMALLOC_MAIN_H
+#include <stdio.h>
+#include <stddef.h>
+#include <assert.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#endif //TPMALLOC_MAIN_H
